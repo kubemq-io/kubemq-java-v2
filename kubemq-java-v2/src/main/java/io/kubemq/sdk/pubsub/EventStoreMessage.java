9e4ea799eb608c1dad4255041b676a38d6e6345d
Kundan Kumar <kkscwcd@gmail.com>
2024-07-24 17:15:55 +0530

    Completed Milestone 1: SDK - Pubsub (Initial SDK release with Pubsub support + simple working example for Events and Events Store)

---
+++ kubemq-java-v2/src/main/java/io/kubemq/sdk/pubsub/EventStoreMessage.java
@@ -0,0 +1,93 @@
+package io.kubemq.sdk.pubsub;
+
+import com.google.protobuf.ByteString;
+import kubemq.Kubemq;
+import lombok.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Represents a message to be stored in the event store of KubeMQ.
+ */
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventStoreMessage {
+    /**
+     * The unique identifier of the message.
+     */
+    private String id;
+
+    /**
+     * The channel to which the message belongs.
+     */
+    private String channel;
+
+    /**
+     * The metadata associated with the message.
+     */
+    private String metadata;
+
+    /**
+     * The body of the message.
+     */
+    private byte[] body;
+
+    /**
+     * The tags associated with the message.
+     */
+    private Map<String, String> tags = new HashMap<>();
+
+    /**
+     * Validates that the necessary fields are present in the message.
+     *
+     * @return The current instance of EventStoreMessage.
+     * @throws IllegalArgumentException If any required field is missing.
+     */
+    public EventStoreMessage validate() {
+        if (channel == null || channel.isEmpty()) {
+            throw new IllegalArgumentException("Event Store message must have a channel.");
+        }
+
+        if (metadata == null && body == null && (tags == null || tags.isEmpty())) {
+            throw new IllegalArgumentException("Event Store message must have at least one of the following: metadata, body, or tags.");
+        }
+
+        return this;
+    }
+
+    /**
+     * Encodes the message into a Kubemq.Event object for transmission.
+     *
+     * @param clientId The client ID to be added as a tag.
+     * @return The encoded pbEvent object.
+     */
+    public Kubemq.Event encode(String clientId) {
+        if (tags == null) {
+            tags = new HashMap<>();
+        }
+        tags.put("x-kubemq-client-id", clientId);
+        return Kubemq.Event.newBuilder()
+                .setEventID(id != null ? id : UUID.randomUUID().toString())
+                .setClientID(clientId)
+                .setChannel(channel)
+                .setMetadata(Optional.ofNullable(metadata).orElse(""))
+                .setBody(ByteString.copyFrom(body))
+                .setStore(true)
+                .putAllTags(Optional.ofNullable(tags).orElse(new HashMap<String, String>()))
+                .build();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("EventStoreMessage: id=%s, channel=%s, metadata=%s, body=%s, tags=%s",
+                id, channel, metadata, body != null ? new String(body) : "", tags);
+    }
+}
+
+
---
+++ kubemq-java-v2/src/main/java/io/kubemq/sdk/pubsub/EventsSubscription.java
@@ -0,0 +1,94 @@
+package io.kubemq.sdk.pubsub;
+
+import kubemq.Kubemq;
+import lombok.*;
+
+import java.util.function.Consumer;
+
+/**
+ * Represents a subscription to events in KubeMQ.
+ */
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventsSubscription {
+
+    /**
+     * The channel to subscribe to.
+     */
+    private String channel;
+
+    /**
+     * The group to subscribe with.
+     */
+    private String group;
+
+    /**
+     * Callback function to be called when an event message is received.
+     */
+    private Consumer<EventMessageReceived> onReceiveEventCallback;
+
+    /**
+     * Callback function to be called when an error occurs.
+     */
+    private Consumer<String> onErrorCallback;
+
+    /**
+     * Raises the onReceiveEventCallback with the given event message.
+     *
+     * @param receivedEvent The received event message.
+     */
+    public void raiseOnReceiveMessage(EventMessageReceived receivedEvent) {
+        if (onReceiveEventCallback != null) {
+            onReceiveEventCallback.accept(receivedEvent);
+        }
+    }
+
+    /**
+     * Raises the onErrorCallback with the given error message.
+     *
+     * @param msg The error message.
+     */
+    public void raiseOnError(String msg) {
+        if (onErrorCallback != null) {
+            onErrorCallback.accept(msg);
+        }
+    }
+
+    /**
+     * Validates the subscription, ensuring that the required fields are set.
+     *
+     * @throws IllegalArgumentException if the channel or onReceiveEventCallback is not set.
+     */
+    public void validate() {
+        if (channel == null || channel.isEmpty()) {
+            throw new IllegalArgumentException("Event subscription must have a channel.");
+        }
+        if (onReceiveEventCallback == null) {
+            throw new IllegalArgumentException("Event subscription must have a onReceiveEventCallback function.");
+        }
+    }
+
+    /**
+     * Encodes the subscription into a KubeMQ Subscribe object.
+     *
+     * @param clientId The client ID to use for the subscription.
+     * @return The encoded KubeMQ Subscribe object.
+     */
+    public Kubemq.Subscribe encode(String clientId) {
+        return Kubemq.Subscribe.newBuilder()
+                .setChannel(channel)
+                .setGroup(group)
+                .setClientID(clientId)
+                .setSubscribeTypeData(Kubemq.Subscribe.SubscribeType.Events)
+                .build();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("EventsSubscription: channel=%s, group=%s", channel, group);
+    }
+}
+
