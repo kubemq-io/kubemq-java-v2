4c067d8d396a88e1328f847792e3b3e890b04f55
Kundan Kumar <kkscwcd@gmail.com>
2024-07-29 18:16:29 +0530

    Milestone 2: "SDK - Queues (Adding Queue supports , Upstream, Downstream + simple working example)" Changed the send Queue Message Stream implementation and example.

--- kubemq-java/src/main/java/io/kubemq/sdk/queues/UpstreamSender.java
+++ kubemq-java/src/main/java/io/kubemq/sdk/queues/UpstreamSender.java
@@ -1,44 +1,64 @@
 package io.kubemq.sdk.queues;
 
+import io.grpc.stub.StreamObserver;
+import io.kubemq.sdk.client.KubeMQClient;
+import kubemq.Kubemq;
 import lombok.Builder;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
 
 @Data
-@Builder
+@Slf4j
 public class UpstreamSender {
 
-    /**
-     * Callback function to be invoked when message send result is received.
-     */
-    private Consumer<UpstreamResponse> onReceiveMessageSendCallback;
+    private StreamObserver<Kubemq.QueuesUpstreamRequest> queuesUpStreamHandler = null;
 
-    /**
-     * Callback function to be invoked when an error occurs.
-     */
-    private Consumer<String> onErrorCallback;
+    public QueueSendResult sendMessage(KubeMQClient kubeMQClient, Kubemq.QueuesUpstreamRequest queueMessage) {
 
-    /**
-     * Invokes the raiseOnReceiveMessage with the given event.
-     *
-     * @param receivedEvent The received event.
-     */
-    public void raiseOnReceiveMessage(UpstreamResponse receivedEvent) {
-        if (onReceiveMessageSendCallback != null) {
-            onReceiveMessageSendCallback.accept(receivedEvent);
+        CompletableFuture<QueueSendResult> futureResponse = new CompletableFuture<>();
+
+        if (queuesUpStreamHandler == null) {
+            StreamObserver<Kubemq.QueuesUpstreamResponse> request = new StreamObserver<Kubemq.QueuesUpstreamResponse>() {
+                @Override
+                public void onNext(Kubemq.QueuesUpstreamResponse messageReceive) {
+                    log.trace("QueuesUpstreamResponse Received Message send result: '{}'", messageReceive);
+                    QueueSendResult qsr = new QueueSendResult();
+                    if(!messageReceive.getIsError()) {
+                        qsr.decode(messageReceive.getResults(0));
+                    }else{
+                        qsr.setIsError(true);
+                        qsr.setError(messageReceive.getError());
+                    }
+                    futureResponse.complete(qsr);
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    log.error("Error in QueuesUpstreamResponse Error message sending: ", t);
+                    UpstreamResponse qpResp = UpstreamResponse.builder()
+                            .error(t.getMessage())
+                            .isError(true).build();
+                    futureResponse.completeExceptionally(t);
+                }
+
+                @Override
+                public void onCompleted() {
+                    log.trace("QueuesUpstreamResponse onCompleted.");
+                }
+            };
+
+            queuesUpStreamHandler = kubeMQClient.getAsyncClient().queuesUpstream(request);
+        }
+        queuesUpStreamHandler.onNext(queueMessage);
+
+        try {
+            return futureResponse.get();
+        } catch (Exception e) {
+            log.error("Error waiting for response: ", e);
+            throw new RuntimeException("Failed to get response", e);
         }
     }
-
-    /**
-     * Invokes the onErrorCallback with the given error message result.
-     *
-     * @param msg The error message.
-     */
-    public void raiseOnError(String msg) {
-        if (onErrorCallback != null) {
-            onErrorCallback.accept(msg);
-        }
-    }
-
 }
