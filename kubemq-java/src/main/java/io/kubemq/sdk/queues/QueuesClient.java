56cadc4b222f5d06114f6ea61e0a24611783a580
Lior Nabat <lior.nabat@gmail.com>
2024-08-03 14:04:00 +0300

    Refactor QueuesClient by removing redundant methods
    Removed several redundant methods such as getQueuesInfo, sendQueuesMessageInBatch, and ackAllQueueMessage to simplify QueuesClient. Updated the main methods to streamline queue operations including sending, receiving, and pulling messages. This cleanup reduces complexity and enhances code maintainability.

--- kubemq-java/src/main/java/io/kubemq/sdk/queues/QueuesClient.java
+++ kubemq-java/src/main/java/io/kubemq/sdk/queues/QueuesClient.java
@@ -3,33 +3,23 @@ package io.kubemq.sdk.queues;
 import io.kubemq.sdk.client.KubeMQClient;
 import io.kubemq.sdk.common.KubeMQUtils;
 import kubemq.Kubemq;
-import kubemq.Kubemq.SendQueueMessageResult;
 import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 /**
- * QueuesClient class represents a client that connects to the KubeMQ server for queue operations.
- *
- * Example usage:
- * <pre>
- * QueuesClient client = QueuesClient.builder().kubeMQClient(new KubeMQClient("localhost:50000")).build();
- * boolean isChannelCreated = client.createQueuesChannel("myChannel");
- * List<QueuesChannel> channels = client.listQueuesChannels("myChannel");
- * client.deleteQueuesChannel("myChannel");
- * </pre>
- *
- * Note: Make sure to set the connection attributes correctly before connecting to the server.
+ * QueuesClient class is a client for interacting with the Queues feature of KubeMQ.
+ * It extends the KubeMQClient class and provides methods for creating, deleting, and listing queues channels,
+ * as well as sending and receiving messages from queues channels.
  */
 @Slf4j
 public class QueuesClient extends KubeMQClient {
 
     @Builder
     public QueuesClient(String address, String clientId, String authToken, boolean tls, String tlsCertFile, String tlsKeyFile,
-                    int maxReceiveSize, int reconnectIntervalSeconds, boolean keepAlive, int pingIntervalInSeconds, int pingTimeoutInSeconds, KubeMQClient.Level logLevel) {
+                        int maxReceiveSize, int reconnectIntervalSeconds, boolean keepAlive, int pingIntervalInSeconds, int pingTimeoutInSeconds, Level logLevel) {
         super(address, clientId, authToken, tls, tlsCertFile, tlsKeyFile, maxReceiveSize, reconnectIntervalSeconds, keepAlive, pingIntervalInSeconds, pingTimeoutInSeconds, logLevel);
     }
 
@@ -42,39 +32,6 @@ public class QueuesClient extends KubeMQClient {
     public boolean createQueuesChannel(String channel) {
         return KubeMQUtils.createChannelRequest(this, this.getClientId(), channel, "queues");
     }
-
-    /**
-     * Gets detailed information of a queue from the KubeMQ server queues.
-     *
-     * @param channelName The name of the queue channel.
-     * @return QueuesDetailInfo containing detailed information about the queue.
-     * @throws IllegalArgumentException if the channel name is null or empty.
-     */
-    public QueuesDetailInfo getQueuesInfo(String channelName) {
-        log.debug("Sending getQueuesInfo");
-        if (channelName == null || channelName.isEmpty()) {
-            throw new IllegalArgumentException("Channel name is required");
-        }
-
-        Kubemq.QueuesInfoRequest request = Kubemq.QueuesInfoRequest.newBuilder()
-                .setRequestID(UUID.randomUUID().toString())
-                .setQueueName(channelName)
-                .build();
-
-        Kubemq.QueuesInfoResponse result = this.getClient().queuesInfo(request);
-        log.debug("QueueInfo Received: {}", result);
-
-        QueuesDetailInfo detailInfo = QueuesDetailInfo.builder()
-                .refRequestID(result.getRefRequestID())
-                .totalQueue(result.getInfo().getTotalQueue())
-                .sent(result.getInfo().getSent())
-                .delivered(result.getInfo().getDelivered())
-                .waiting(result.getInfo().getWaiting())
-                .queues(result.getInfo().getQueuesList())
-                .build();
-        return detailInfo;
-    }
-
     /**
      * Deletes a queues channel.
      *
@@ -96,60 +53,12 @@ public class QueuesClient extends KubeMQClient {
     }
 
     /**
-     * Sends a message to the KubeMQ server queues.
-     *
-     * @param message The message to be sent.
-     * @return QueueSendResult The result of the message send operation.
-     */
-    public QueueSendResult sendQueuesMessage(QueueMessage message) {
-        log.debug("Sending queues message");
-        message.validate();
-        Kubemq.QueueMessage queueMessage = message.encodeMessage(this.getClientId());
-        SendQueueMessageResult result = this.getClient().sendQueueMessage(queueMessage);
-        log.debug("Queue message sent: {}", result);
-        return new QueueSendResult().decode(result);
-    }
-
-    /**
-     * Sends a batch of messages to the KubeMQ server queues.
-     *
-     * @param queueMessages The list of messages to be sent.
-     * @param batchId  The batch ID, if null a new UUID will be generated.
-     * @return QueueMessagesBatchSendResult The result of the batch send operation.
-     */
-    public QueueMessagesBatchSendResult sendQueuesMessageInBatch(List<QueueMessage> queueMessages, String batchId) {
-        log.debug("Sending queues messages in batch");
-        // Converts a list of QueueMessageWrapper objects to a list of Kubemq.QueueMessage objects.
-        List<Kubemq.QueueMessage> messages = new ArrayList<>();
-        for (QueueMessage msg : queueMessages) {
-            msg.validate();
-            messages.add(msg.encodeMessage(this.getClientId()));
-        }
-        Kubemq.QueueMessagesBatchRequest queueMessagesBatchRequest = Kubemq.QueueMessagesBatchRequest.newBuilder()
-                .setBatchID(batchId != null ? batchId : UUID.randomUUID().toString())
-                .addAllMessages(messages)
-                .build();
-
-        Kubemq.QueueMessagesBatchResponse batchMessageResponse = this.getClient().sendQueueMessagesBatch(queueMessagesBatchRequest);
-        log.debug("Batch queue messages sent: {}", batchMessageResponse);
-        QueueMessagesBatchSendResult batchSendResult = QueueMessagesBatchSendResult.builder().build();
-        batchSendResult.setBatchId(batchMessageResponse.getBatchID());
-        batchSendResult.setHaveErrors(batchMessageResponse.getHaveErrors());
-
-        for (SendQueueMessageResult msgRes : batchMessageResponse.getResultsList()) {
-            batchSendResult.getResults().add(new QueueSendResult().decode(msgRes));
-        }
-        return batchSendResult;
-    }
-
-
-    /**
      * Sends messages to a queues channel.
      *
      * @param queueMessage message to send in queue
-     * @return UpstreamResponse The stream from the upstream.
+     * @return QueueSendResult The result of the send operation.
      */
-    public QueueSendResult sendQueuesMessageUpStream(QueueMessage queueMessage) {
+    public QueueSendResult sendQueuesMessage(QueueMessage queueMessage) {
         queueMessage.validate();
       return new QueueStreamHelper().sendMessage(this, queueMessage.encode(this.getClientId()));
     }
@@ -158,30 +67,100 @@ public class QueuesClient extends KubeMQClient {
      * Receives messages downstream from a queues channel.
      *
      * @param queuesPollRequest Queues Poll request to poll the messages from queue
-
+     * @return QueuesPollResponse The result of the poll operation
      */
-    public QueuesPollResponse receiveQueuesMessagesDownStream(QueuesPollRequest queuesPollRequest) {
+    public QueuesPollResponse receiveQueuesMessages(QueuesPollRequest queuesPollRequest) {
         queuesPollRequest.validate();
         return new QueueStreamHelper().receiveMessage(this, queuesPollRequest);
     }
 
     /**
-     * Acknowledges all messages in a queue.
+     * Get waiting messages from a queue.
      *
-     * @param requestId        The request ID.
-     * @param queueName        The name of the queue.
-     * @param waitTimeInSeconds The wait time in seconds.
-     * @return QueueMessageAcknowledgment The acknowledgment response.
+     * @param channel The name of the queue channel.
+     * @param maxMessages The maximum number of messages to pull.
+     * @param waitTimeoutInSeconds The maximum amount of time to wait for messages in seconds.
+     * @return A QueueMessagesWaiting object containing the waiting messages.
+     * @throws IllegalArgumentException if channel is null, maxMessages is less than 1, or waitTimeoutInSeconds is less than 1.
      */
-    public QueueMessageAcknowledgment ackAllQueueMessage(String requestId, String queueName, int waitTimeInSeconds) {
-        Kubemq.AckAllQueueMessagesRequest ackRequest = Kubemq.AckAllQueueMessagesRequest.newBuilder()
-                .setRequestID(requestId)
-                .setChannel(queueName)
+    public QueueMessagesWaiting waiting(String channel, int maxMessages, int waitTimeoutInSeconds) {
+        log.debug("Get waiting messages from queue: {}", channel);
+        if (channel == null) {
+            throw new IllegalArgumentException("channel cannot be null.");
+        }
+        if (maxMessages < 1) {
+            throw new IllegalArgumentException("maxMessages must be greater than 0.");
+        }
+        if (waitTimeoutInSeconds < 1) {
+            throw new IllegalArgumentException("waitTimeoutInSeconds must be greater than 0.");
+        }
+
+        Kubemq.ReceiveQueueMessagesRequest rcvQueuesMessageReq = Kubemq.ReceiveQueueMessagesRequest.newBuilder()
+                .setRequestID(UUID.randomUUID().toString())
                 .setClientID(this.getClientId())
-                .setWaitTimeSeconds(waitTimeInSeconds)
+                .setChannel(channel)
+                .setMaxNumberOfMessages(maxMessages)
+                .setWaitTimeSeconds(waitTimeoutInSeconds)
+                .setIsPeak(true)
                 .build();
-        Kubemq.AckAllQueueMessagesResponse ackResp = this.getClient().ackAllQueueMessages(ackRequest);
-        return QueueMessageAcknowledgment.decode(ackResp);
+        Kubemq.ReceiveQueueMessagesResponse rcvQueuesMessageResponse = this.getClient().receiveQueueMessages(rcvQueuesMessageReq);
+        QueueMessagesWaiting rcvWaitingMessages = QueueMessagesWaiting.builder()
+                .isError(rcvQueuesMessageResponse.getIsError())
+                .error(rcvQueuesMessageResponse.getError())
+                .build();
+        if (rcvQueuesMessageResponse.getMessagesList().isEmpty()) {
+            return rcvWaitingMessages;
+        }
+        log.debug("Waiting messages count: {}", rcvQueuesMessageResponse.getMessagesList().size());
+        for (Kubemq.QueueMessage queueMessage : rcvQueuesMessageResponse.getMessagesList()) {
+            rcvWaitingMessages.getMessages().add(QueueMessageWaitingPulled.decode(queueMessage,this.getClientId()));
+        }
+        return rcvWaitingMessages;
+    }
+
+
+    /**
+     * Pulls messages from a queue.
+     *
+     * @param channel The name of the queue channel.
+     * @param maxMessages The maximum number of messages to pull.
+     * @param waitTimeoutInSeconds The maximum amount of time to wait for messages in seconds.
+     * @return A QueueMessagesPulled object containing the pulled messages.
+     * @throws IllegalArgumentException if channel is null, maxMessages is less than 1, or waitTimeoutInSeconds is less than 1.
+     */
+    public QueueMessagesPulled pull(String channel, int maxMessages, int waitTimeoutInSeconds) {
+        log.debug("Pulling messages from queue: {}", channel);
+        if (channel == null) {
+            throw new IllegalArgumentException("channel cannot be null.");
+        }
+        if (maxMessages < 1) {
+            throw new IllegalArgumentException("maxMessages must be greater than 0.");
+        }
+        if (waitTimeoutInSeconds < 1) {
+            throw new IllegalArgumentException("waitTimeoutInSeconds must be greater than 0.");
+        }
+
+        Kubemq.ReceiveQueueMessagesRequest rcvQueuesMessageReq = Kubemq.ReceiveQueueMessagesRequest.newBuilder()
+                .setRequestID(UUID.randomUUID().toString())
+                .setClientID(this.getClientId())
+                .setChannel(channel)
+                .setMaxNumberOfMessages(maxMessages)
+                .setWaitTimeSeconds(waitTimeoutInSeconds)
+                .setIsPeak(false)
+                .build();
+        Kubemq.ReceiveQueueMessagesResponse rcvQueuesMessageResponse = this.getClient().receiveQueueMessages(rcvQueuesMessageReq);
+        QueueMessagesPulled rcvPulledMessages = QueueMessagesPulled.builder()
+                .isError(rcvQueuesMessageResponse.getIsError())
+                .error(rcvQueuesMessageResponse.getError())
+                .build();
+        if (rcvQueuesMessageResponse.getMessagesList().isEmpty()) {
+            return rcvPulledMessages;
+        }
+        log.debug("Pulled messages count: {}", rcvQueuesMessageResponse.getMessagesList().size());
+        for (Kubemq.QueueMessage queueMessage : rcvQueuesMessageResponse.getMessagesList()) {
+            rcvPulledMessages.getMessages().add(QueueMessageWaitingPulled.decode(queueMessage,this.getClientId()));
+        }
+        return rcvPulledMessages;
     }
 
 }
