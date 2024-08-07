99d06b05cd9e17de2c8f5e20d8c76983091cf6d8
Kundan Kumar <kkscwcd@gmail.com>
2024-08-07 23:08:52 +0530

    Fixed null pointer issue

--- kubemq-java/src/main/java/io/kubemq/sdk/pubsub/EventStreamHelper.java
+++ kubemq-java/src/main/java/io/kubemq/sdk/pubsub/EventStreamHelper.java
@@ -13,26 +13,34 @@ import java.util.concurrent.CompletableFuture;
 public class EventStreamHelper {
 
     private StreamObserver<Kubemq.Event> queuesUpStreamHandler = null;
+    private StreamObserver<Kubemq.Result> resultStreamObserver;
+    private CompletableFuture<EventSendResult> futureResponse;
+
+    public EventStreamHelper() {
+        futureResponse = new CompletableFuture<>();
+        this.resultStreamObserver =  new StreamObserver<Kubemq.Result>() {
+            @Override
+            public void onNext(Kubemq.Result result) {
+                log.debug("Received EventSendResult: '{}'", result);
+                futureResponse.complete(EventSendResult.decode(result));
+            }
+            @Override
+            public void onError(Throwable t) {
+                log.error("Error in EventSendResult: ", t);
+                EventSendResult sendResult = new EventSendResult();
+                sendResult.setError(t.getMessage());
+                futureResponse.complete(sendResult);
+            }
+            @Override
+            public void onCompleted() {
+                log.debug("EventSendResult onCompleted.");
+            }
+        };
+    }
 
     public void sendEventMessage(KubeMQClient kubeMQClient, Kubemq.Event event) {
 
         if (queuesUpStreamHandler == null) {
-            StreamObserver<Kubemq.Result> resultStreamObserver = new StreamObserver<Kubemq.Result>() {
-                @Override
-                public void onNext(Kubemq.Result result) {
-                    log.debug("Received EventSendResult: '{}'", result);
-                }
-
-                @Override
-                public void onError(Throwable t) {
-                    log.error("Error in EventSendResult: ", t);
-                }
-
-                @Override
-                public void onCompleted() {
-                    log.debug("EventSendResult onCompleted.");
-                }
-            };
             queuesUpStreamHandler = kubeMQClient.getAsyncClient().sendEventsStream(resultStreamObserver);
         }
         queuesUpStreamHandler.onNext(event);
@@ -40,29 +48,8 @@ public class EventStreamHelper {
     }
 
     public EventSendResult sendEventStoreMessage(KubeMQClient kubeMQClient, Kubemq.Event event) {
-        CompletableFuture<EventSendResult> futureResponse = new CompletableFuture<>();
 
         if (queuesUpStreamHandler == null) {
-            StreamObserver<Kubemq.Result> resultStreamObserver = new StreamObserver<Kubemq.Result>() {
-                @Override
-                public void onNext(Kubemq.Result result) {
-                    log.debug("Received EventSendResult: '{}'", result);
-                    futureResponse.complete(EventSendResult.decode(result));
-                }
-
-                @Override
-                public void onError(Throwable t) {
-                    log.error("Error in EventSendResult: ", t);
-                    EventSendResult sendResult = new EventSendResult();
-                     sendResult.setError(t.getMessage());
-                    futureResponse.complete(sendResult);
-                }
-
-                @Override
-                public void onCompleted() {
-                    log.debug("EventSendResult onCompleted.");
-                }
-            };
             queuesUpStreamHandler = kubeMQClient.getAsyncClient().sendEventsStream(resultStreamObserver);
         }
         queuesUpStreamHandler.onNext(event);
