2955d841da44dbd6f4511e64556bf59e62670ee5
kkscwcd <kkscwcd@gmail.com>
2024-10-03 15:11:07 +0530

    Strema close implementation here are the rules: * 		If request has AutoAck close stream after receiving. * 		If auto ack is false and no messages received, close stream after receiving. * 		if there are messages received , create array of all the message id. * 		for every action, remove the message id that is relvelvant. after each action, check if the array is empty, if yes, close the stream. * 		if AckAll , RejectAll, RequeueAll, you should close the stream after it (as you will clean all the array)

--- kubemq-java/src/main/java/io/kubemq/sdk/queues/QueuesPollResponse.java
+++ kubemq-java/src/main/java/io/kubemq/sdk/queues/QueuesPollResponse.java
@@ -7,9 +7,7 @@ import kubemq.Kubemq.QueuesDownstreamResponse;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @Getter
 @Setter
@@ -24,24 +22,34 @@ public class QueuesPollResponse {
     private List<QueueMessageReceived> messages = new ArrayList<>();
     private String error;
     private boolean isError;
+    @Getter
     private boolean isTransactionCompleted;
     @Builder.Default
     private List<Long> activeOffsets = new ArrayList<>();
     private StreamObserver<QueuesDownstreamRequest> responseHandler;
     private String receiverClientId;
     private int visibilitySeconds;
+    @Getter
     private boolean isAutoAcked;
+
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    private Queue<String> msgQueue = new LinkedList<>();
+
 
     public void ackAll() {
         doOperation(QueuesDownstreamRequestType.AckAll, null);
+        this.closeStream();
     }
 
     public void rejectAll() {
         doOperation(QueuesDownstreamRequestType.NAckAll, null);
+        this.closeStream();
     }
 
     public void reQueueAll(String channel) {
         doOperation(QueuesDownstreamRequestType.ReQueueAll, channel);
+        this.closeStream();
     }
 
     private void doOperation(QueuesDownstreamRequestType requestType, String reQueueChannel) {
@@ -76,47 +84,45 @@ public class QueuesPollResponse {
             }
     }
 
-    public static QueuesPollResponse decode(
+    public  QueuesPollResponse decode(
             QueuesDownstreamResponse response,
             String receiverClientId,
             StreamObserver<QueuesDownstreamRequest> responseHandler,
             int visibilitySeconds,
             boolean isAutoAcked
     ) {
-        QueuesPollResponse pollResponse = new QueuesPollResponse();
-        pollResponse.refRequestId = response.getRefRequestId();
-        pollResponse.transactionId = response.getTransactionId();
-        pollResponse.error = response.getError();
-        pollResponse.isError = response.getIsError();
-        pollResponse.isTransactionCompleted = response.getTransactionComplete();
-        pollResponse.activeOffsets.addAll(response.getActiveOffsetsList());
-        pollResponse.responseHandler = responseHandler;
-        pollResponse.receiverClientId = receiverClientId;
-        pollResponse.visibilitySeconds = visibilitySeconds;
-        pollResponse.isAutoAcked = isAutoAcked;
+        //QueuesPollResponse pollResponse = new QueuesPollResponse();
+        this.refRequestId = response.getRefRequestId();
+        this.transactionId = response.getTransactionId();
+        this.error = response.getError();
+        this.isError = response.getIsError();
+        this.isTransactionCompleted = response.getTransactionComplete();
+        this.activeOffsets.addAll(response.getActiveOffsetsList());
+        this.responseHandler = responseHandler;
+        this.receiverClientId = receiverClientId;
+        this.visibilitySeconds = visibilitySeconds;
+        this.isAutoAcked = isAutoAcked;
 
         response.getMessagesList().forEach(message -> {
-            QueueMessageReceived receivedMessage = QueueMessageReceived.decode(
+            QueueMessageReceived receivedMessage = new QueueMessageReceived().decode(
                     message,
                     response.getTransactionId(),
                     response.getTransactionComplete(),
                     receiverClientId,
                     responseHandler,
                     visibilitySeconds,
-                    isAutoAcked
+                    isAutoAcked,
+                    this
             );
-            pollResponse.messages.add(receivedMessage);
+            this.messages.add(receivedMessage);
+            this.msgQueue.add(receivedMessage.getId());
         });
+        //If auto ack is true OR auto ack is false and no messages received, close stream after receiving.
+        if(isAutoAcked == false || this.messages.isEmpty()){
+            this.closeStream();
+        }
 
-        return pollResponse;
-    }
-
-    @Override
-    public String toString() {
-        return String.format(
-                "QueuesPollResponse: refRequestId=%s, transactionId=%s, error=%s, isError=%s, " +
-                        "isTransactionCompleted=%s, activeOffsets=%s, messages=%s",
-                refRequestId, transactionId, error, isError, isTransactionCompleted, activeOffsets, messages);
+        return this;
     }
 
     // Method to add tasks to the queue
@@ -132,7 +138,34 @@ public class QueuesPollResponse {
             }
         });
     }
+
+    private void closeStream(){
+        if(responseHandler != null){
+            responseHandler.onCompleted();
+        }
+        msgQueue.clear();
+    }
+
+    public void checkAndCloseStream(String id) {
+        if (msgQueue.contains(id)) {
+            msgQueue.remove(id);
+        }
+        // If the queue is empty after processing, trigger another function
+        if (msgQueue.isEmpty()) {
+            this.closeStream();
+        }
+    }
+
     public boolean isError() {
         return this.isError;
     }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "QueuesPollResponse: refRequestId=%s, transactionId=%s, error=%s, isError=%s, " +
+                        "isTransactionCompleted=%s, activeOffsets=%s, messages=%s",
+                refRequestId, transactionId, error, isError, isTransactionCompleted, activeOffsets, messages);
+    }
+
 }
