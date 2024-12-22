167f40e553baa2f830e7e8d09f86d6fc066a3d64
Lior Nabat <lior.nabat@gmail.com>
2024-12-22 13:50:02 +0200

    Refactor message handling logic in QueuesPollResponse.
    Replaced `responseHandler` with `requestSender` for better abstraction and concurrency. Introduced `receivedMessages` map to track and manage message states efficiently. Removed unused methods and redundant code to simplify the class structure.

--- kubemq-java/src/main/java/io/kubemq/sdk/queues/QueuesPollResponse.java
+++ kubemq-java/src/main/java/io/kubemq/sdk/queues/QueuesPollResponse.java
@@ -8,6 +8,7 @@ import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Getter
 @Setter
@@ -26,31 +27,44 @@ public class QueuesPollResponse {
     private boolean isTransactionCompleted;
     @Builder.Default
     private List<Long> activeOffsets = new ArrayList<>();
-    private StreamObserver<QueuesDownstreamRequest> responseHandler;
+
     private String receiverClientId;
     private int visibilitySeconds;
     @Getter
     private boolean isAutoAcked;
 
+//    @Getter(AccessLevel.NONE)
+//    @Setter(AccessLevel.NONE)
+//    @Builder.Default
+//    private Queue<String> msgQueue = new LinkedList<>();
+
     @Getter(AccessLevel.NONE)
     @Setter(AccessLevel.NONE)
-    @Builder.Default
-    private Queue<String> msgQueue = new LinkedList<>();
+    private RequestSender requestSender;
 
 
+    private final Map<String,String> receivedMessages = new ConcurrentHashMap<>();
+
+    void complete(RequestSender requestSender) {
+        if (isAutoAcked) {
+            return;
+        }
+        this.requestSender = requestSender;
+        for (QueueMessageReceived message : messages) {
+            message.setRequestSender(requestSender);
+            receivedMessages.put(message.getId(), message.getId());
+        }
+    }
     public void ackAll() {
         doOperation(QueuesDownstreamRequestType.AckAll, null);
-        this.closeStream();
     }
 
     public void rejectAll() {
         doOperation(QueuesDownstreamRequestType.NAckAll, null);
-        this.closeStream();
     }
 
     public void reQueueAll(String channel) {
         doOperation(QueuesDownstreamRequestType.ReQueueAll, channel);
-        this.closeStream();
     }
 
     private void doOperation(QueuesDownstreamRequestType requestType, String reQueueChannel) {
@@ -60,103 +74,39 @@ public class QueuesPollResponse {
         if (isTransactionCompleted) {
             throw new IllegalStateException("Transaction is already completed");
         }
-        if (responseHandler == null) {
+        if (requestSender == null) {
             throw new IllegalStateException("Response handler is not set");
         }
 
-            QueuesDownstreamRequest.Builder requestBuilder = QueuesDownstreamRequest.newBuilder()
-                    .setRequestID(UUID.randomUUID().toString())
-                    .setClientID(receiverClientId)
-                    .setRequestTypeData(requestType)
-                    .setRefTransactionId(transactionId)
-                    .addAllSequenceRange(activeOffsets);
+        QueuesDownstreamRequest.Builder requestBuilder = QueuesDownstreamRequest.newBuilder()
+                .setRequestID(UUID.randomUUID().toString())
+                .setClientID(receiverClientId)
+                .setRequestTypeData(requestType)
+                .setRefTransactionId(transactionId)
+                .addAllSequenceRange(activeOffsets);
 
-            if (reQueueChannel != null && requestType == QueuesDownstreamRequestType.ReQueueAll) {
-                requestBuilder.setReQueueChannel(reQueueChannel);
-            }
+        if (reQueueChannel != null && requestType == QueuesDownstreamRequestType.ReQueueAll) {
+            requestBuilder.setReQueueChannel(reQueueChannel);
+        }
 
-            QueuesDownstreamRequest request = requestBuilder.build();
-            this.addTaskToThreadSafeQueue(request);
+        QueuesDownstreamRequest request = requestBuilder.build();
+        if (requestSender != null) {
+            requestSender.send(request);
+        }
 
+        isTransactionCompleted = true;
+        for (QueueMessageReceived message : messages) {
+            message.markTransactionCompleted();
+        }
+        receivedMessages.clear();
+    }
+
+
+
+    public void markMessageCompleted(String id) {
+        receivedMessages.remove(id);
+        if (receivedMessages.isEmpty()) {
             isTransactionCompleted = true;
-            // Mark all messages as completed
-            for (QueueMessageReceived message : messages) {
-                message.markTransactionCompleted();
-            }
-    }
-
-    public  QueuesPollResponse decode(
-            QueuesDownstreamResponse response,
-            String receiverClientId,
-            StreamObserver<QueuesDownstreamRequest> responseHandler,
-            int visibilitySeconds,
-            boolean isAutoAcked
-    ) {
-
-        this.refRequestId = response.getRefRequestId();
-        this.transactionId = response.getTransactionId();
-        this.error = response.getError();
-        this.isError = response.getIsError();
-        this.isTransactionCompleted = response.getTransactionComplete();
-        this.activeOffsets.addAll(response.getActiveOffsetsList());
-        this.responseHandler = responseHandler;
-        this.receiverClientId = receiverClientId;
-        this.visibilitySeconds = visibilitySeconds;
-        this.isAutoAcked = isAutoAcked;
-
-        response.getMessagesList().forEach(message -> {
-            QueueMessageReceived receivedMessage = new QueueMessageReceived().decode(
-                    message,
-                    response.getTransactionId(),
-                    response.getTransactionComplete(),
-                    receiverClientId,
-                    responseHandler,
-                    visibilitySeconds,
-                    isAutoAcked,
-                    this
-            );
-            this.messages.add(receivedMessage);
-            this.msgQueue.add(receivedMessage.getId());
-        });
-        //If auto ack is true OR auto ack is false and no messages received, close stream after receiving.
-        if(isAutoAcked == true || this.messages.isEmpty()){
-            this.closeStream();
-        }
-
-        return this;
-    }
-
-    // Method to add tasks to the queue
-    private void addTaskToThreadSafeQueue(QueuesDownstreamRequest request) {
-        QueueDownStreamProcessor.addTask(() -> {
-            synchronized (responseHandler) {
-                try {
-                    responseHandler.onNext(request);
-                    log.debug("{} message: {}", request.getRequestTypeData(), request.getRequestID());
-                } catch (Exception e) {
-                    log.error("Error processing {}: {}", request.getRequestTypeData(), e.getMessage());
-                }
-            }
-        });
-    }
-
-    private void closeStream(){
-        if(responseHandler != null){
-            responseHandler.onCompleted();
-        }
-
-        if (msgQueue !=null && !msgQueue.isEmpty()) {
-            msgQueue.clear();
-        }
-    }
-
-    public void checkAndCloseStream(String id) {
-        if (msgQueue !=null && msgQueue.contains(id)) {
-            msgQueue.remove(id);
-        }
-        // If the queue is empty after processing, trigger another function
-        if (msgQueue ==null || msgQueue.isEmpty()) {
-            this.closeStream();
         }
     }
 
